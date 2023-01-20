6ef16b06306d28de884fe6759025d749047540d5
ink0rr <tomflynnbeltsazar@gmail.com>
2023-01-21 03:50:36 +0700

    Disable logger

--- server.ts
+++ server.ts
@@ -5,19 +5,19 @@ import { router } from "./router.ts";
 const app = new Application();
 
 // Logger
-app.use(async (ctx, next) => {
-  await next();
-  const rt = ctx.response.headers.get("X-Response-Time");
-  console.log(`${ctx.request.method} ${ctx.request.url} - ${rt}`);
-});
+// app.use(async (ctx, next) => {
+//   await next();
+//   const rt = ctx.response.headers.get("X-Response-Time");
+//   console.log(`${ctx.request.method} ${ctx.request.url} - ${rt}`);
+// });
 
 // Timing
-app.use(async (ctx, next) => {
-  const start = Date.now();
-  await next();
-  const ms = Date.now() - start;
-  ctx.response.headers.set("X-Response-Time", `${ms}ms`);
-});
+// app.use(async (ctx, next) => {
+//   const start = Date.now();
+//   await next();
+//   const ms = Date.now() - start;
+//   ctx.response.headers.set("X-Response-Time", `${ms}ms`);
+// });
 
 app.use(oakCors());
 app.use(router.routes());
